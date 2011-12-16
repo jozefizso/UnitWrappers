6b946cada40dfc8c5bc63d7bea282cbd9931f780
Dzmitry Lahoda <asd.and.Rizzo@gmail.com>
2011-12-16 18:10:32 +0300

    -added initial background worker

---
+++ UnitWrappers/System/ComponentModel/IBackgroundWorker.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel;
+
+namespace UnitWrappers.System.ComponentModel
+{
+    public interface IBackgroundWorker
+    {
+        [Browsable(false)]
+        bool CancellationPending { get; }
+
+        [Browsable(false)]
+        bool IsBusy { get; }
+
+        [DefaultValue(false)]
+        bool WorkerReportsProgress { get; set; }
+
+        [DefaultValue(false)]
+        bool WorkerSupportsCancellation { get; set; }
+
+        event DoWorkEventHandler DoWork;
+        event ProgressChangedEventHandler ProgressChanged;
+        event RunWorkerCompletedEventHandler RunWorkerCompleted;
+        void CancelAsync();
+        void ReportProgress(int percentProgress);
+        void ReportProgress(int percentProgress, object userState);
+        void RunWorkerAsync();
+        void RunWorkerAsync(object argument);
+    }
+}
