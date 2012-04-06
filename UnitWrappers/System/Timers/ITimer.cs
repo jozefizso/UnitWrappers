87fd42cfeff2fe142ff01eb3e38894b1497a3696
Dzmitry Lahoda <asd.and.Rizzo@gmail.com>
2012-04-06 13:10:47 +0300

    added time

---
+++ UnitWrappers/System/Timers/ITimer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Security.Permissions;
+using System.Timers;
+
+namespace UnitWrappers.System.Timers
+{
+
+
+    public interface ITimer : ISupportInitialize, IDisposable
+    {
+        event ElapsedEventHandler Elapsed;
+
+        double Interval { get; set; }
+
+        bool AutoReset { get; set; }
+
+        bool Enabled { get; set; }
+
+        void Start();
+    }
+}
